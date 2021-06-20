38221c3b9eb41fcf1bbbe1a73ed38a2b86984363
sunghye-on <opei71@naver.com>
2021-06-20 17:28:08 +0900

    삭제 기능 추가

--- memo.js
+++ memo.js
@@ -5,12 +5,18 @@ const createText = (data) => document.createTextNode(data);
 chrome.storage.sync.get(["memoList"], (data) => {
   let memoList = JSON.parse(data.memoList);
 
-  let memoDiv = getEleById("memo");
+  let memoDiv = getEleById("memoContaienr");
 
   for (const memo of memoList) {
-    let memoP = createEle("p");
+    let memoP = createEle("div");
+    memoP.setAttribute("class", "memoText");
     let memoText = createText(memo);
     memoP.appendChild(memoText);
     memoDiv.appendChild(memoP);
   }
+});
+
+getEleById("resetMemo").addEventListener("click", () => {
+  chrome.storage.sync.clear();
+  location.reload();
 });
